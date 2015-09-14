a744cc87060c42f0782b4e4860874cfc67bc51d1
mouchi <786474202@qq.com>
2015-09-14 15:04:26 +0800

    查找目标文件
    C语言编写的主函数能够查找特定的文件

--- parel.c
+++ parel.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
+#include <dir.h>
 
+int main()
+{
+	struct ffblk ffblk;
+	int done;
+	printf("Directory listing of *.* \n");
+	done=findfirst("*.*",&ffblk,0);
+	while(!done)
+	{
+		printf("%s\n",ffblk.ff_name);
+		done=findnext(&ffblk);
+	}
+	return 0;
+}
