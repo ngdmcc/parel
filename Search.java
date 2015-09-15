2cbd19d62f0e29cdd1d00d5feafd631b1415c4d8
mouchi <786474202@qq.com>
2015-09-15 11:39:34 +0800

    在Search中调用shell脚本程序
    如题，成功调用shell脚本生成了相应的文件

--- Search.java
+++ Search.java
@@ -1,4 +1,9 @@
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 public class Search{
 	// file info
 	private static StringBuffer fileInfo;
@@ -15,18 +20,31 @@ public class Search{
 		// 查找目录
 		File dir = new File(".");
 		// 要查找的关键字
-		String key = "parel.c";
+		String key = "tarfile";
+		//最后一批打包的文件包
+		String last = "lastpack";
 		// 打印文件夹信息
 		printAllInfo(dir);
-		System.out.println("/nsearch key : " + key);
+		System.out.println("\nsearch key : " + key);
 		System.out.println("search results : ");
 		for (File file : fm) {
 			if (file.getName().indexOf(key) >= 0) {
 				if (file.isFile()) {
 					System.out.println("file : " + file.getName() + "   ");
+					System.out.println("该批次文件已经到达，调用解包脚本");
+					callShell("sh untar.sh");
 				} else if (file.isDirectory()) {
 					System.out.println("dir : " + file.getName() + "   ");
 				}
+			}
+			if (file.getName().indexOf(last) >= 0){
+			if (file.isFile()){
+				System.out.println("file:"+file.getName()+" ");
+				System.out.println("最后一批文件到达，调用解包脚本，并且退出整个程序，输出程序结束的提示！");
+				callShell("sh untar.sh");
+			}else if(file.isDirectory()){
+				System.out.println("dir:"+file.getName()+"   ");
+			}
 			}
 		}
 	}
@@ -50,5 +68,17 @@ public class Search{
 		System.out.println("contains : ");
 		System.out.println("file ---> " + fileInfo);
 		System.out.println("dir  ---> " + dirInfo);
+	}
+	//调用shell脚本的函数，成功返回0，失败则会出现报错信息
+		public static void callShell(String shellString) {
+		try {
+			Process process = Runtime.getRuntime().exec(shellString);
+			int exitValue = process.waitFor();
+			if (0 != exitValue) {
+				System.out.println("call shell failed. error code is :" + exitValue);
+			}
+		} catch (Throwable e) {
+			System.out.println("call shell failed. " + e);
+		}
 	}
 }
