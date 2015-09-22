d16f66a833e12fe8ecad9f0ac08fefcd430bc221
mouchi <786474202@qq.com>
2015-09-22 11:58:30 +0800

    添加log文件的多条记录
    期望从log日志中，提取文件名和MD5值进行校验

---
+++ LogTest.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class LogTest{
+	public static void readFileByLines(String fileName) {
+        File file = new File(fileName);
+        BufferedReader reader = null;
+		String reStr= null;
+        try {
+            //System.out.println("以行为单位读取文件内容，一次读一整行：");
+            reader = new BufferedReader(new FileReader(file));
+            String tempString = null;
+            int line = 1;
+            // 一次读入一行，直到读入null为文件结束
+            while ((tempString = reader.readLine()) != null) {
+                // 显示行号
+                System.out.println("line " + line + ": " + tempString);
+				//System.out.println(tempString);
+                //reStr = tempString;
+				line++;
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
+		//return reStr;
+    }
+	public static void main(String[] args)
+	{
+		String  logfile = "log.20";
+		readFileByLines(logfile);
+	}
+}
